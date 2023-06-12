b4554f7e5c5804a59640d5bea8627680d9a23341
Nicola Borghi <nicoborghi@outlook.it>
2023-06-12 15:36:55 +0200

    optional normalization in Bias.py

--- CHIMERA/Bias.py
+++ CHIMERA/Bias.py
@@ -4,12 +4,73 @@
 #
 
 import os, sys
+import numpy as np
 import CHIMERA.chimeraUtils as chimeraUtils
 
-from CHIMERA.Mock import MockGWInjectionsGWFAST
 
 import logging
 log = logging.getLogger(__name__)
+
+
+
+class MockGWInjectionsGWFAST():
+    
+    def __init__(self, 
+                 injections_df, 
+                 N_gen,
+                 dist_unit="Gpc", 
+                 snr_th=None,
+                 Tobs=1, 
+                 
+                #DeltaOm=None,
+                 #nInjUse=None,  
+                 ):
+        
+        self.N_gen      = N_gen
+        self.dist_unit  = dist_unit
+        self.m1z, self.m2z, self.dL, self.log_weights_sel  = self._load_data(injections_df, snr_th)
+        self.logN_gen   = np.log(self.N_gen)
+
+        assert (self.m1z > 0).all()
+        assert (self.m2z > 0).all()
+        assert (self.dL > 0).all()
+        assert(self.m2z<=self.m1z).all()
+    
+        self.Tobs=Tobs
+        self.spins = []
+        
+        print('Obs time: %s' %self.Tobs )
+        self.condition = np.full(self.m1z.shape, True)
+        
+    def get_theta(self):
+        return np.array( [self.m1z, self.m2z, self.dL  ] ) 
+    
+    
+    def _load_data(self, injections_df, snr_th=None):
+        
+        log_weights_sel = injections_df['log_p_draw_nospin']
+
+        self.keep = np.full(len(log_weights_sel), True)
+
+        if snr_th is not None:
+            self.keep = self.keep & (injections_df['SNR_net'] > snr_th)
+            print('Injections SNR threshold set to %s' %snr_th)
+
+        print('Total detected injections: %s' %self.keep.sum())
+
+        if not "m1_det" in injections_df.keys():
+            dL   = injections_df['dL'] # Gpc
+            z    = injections_df['z']
+            m1z  = injections_df['m1src']*(1.+z)
+            m2z  = injections_df['m2src']*(1.+z)
+        else:
+            m1z = injections_df['m1_det']
+            m2z = injections_df['m2_det']
+            dL  = injections_df['dL'] # Gpc
+
+
+        return m1z[self.keep], m2z[self.keep], dL[self.keep],  log_weights_sel[self.keep] 
+
 
 class SelectionBias():
 
@@ -21,14 +82,14 @@ class SelectionBias():
 
         self.MGCP_dir = MGCP_dir
 
-
         # Dictionary to convert CHIMERA parameter names to MGCP parameter names
         self.key2MGCP = {'H0':'H0','Om0':'Om','gamma':'alphaRedshift','kappa':'betaRedshift', 
                 'zp':'zp','R0':'R0','lambdaPeak':'lambdaPeak','beta':'beta','alpha':'alpha',
                 'deltam':'deltam','ml':'ml','mh':'mh','muMass':'muMass','sigmaMass':'sigmaMass'}
 
 
-    def load_pop(self, fname_inj, lambda_cosmo, lambda_rate, lambda_mass, dist_unit='Gpc'):
+    def load_pop(self, fname_inj, lambda_cosmo, lambda_rate, lambda_mass,
+                 dist_unit='Gpc', snr_th=None, N_gen_tot=50000000):
 
         sys.path.append(self.MGCP_dir)
         from MGCosmoPop.population.astro.astroMassDistribution import PowerLawPlusPeakMass
@@ -42,7 +103,7 @@ class SelectionBias():
         self.SelectionBiasInjections = SelectionBiasInjections
 
         injections_load = chimeraUtils.load_data_h5(fname_inj)
-        self.injMock    = MockGWInjectionsGWFAST(injections_load,  N_gen = 50000000)
+        self.injMock    = MockGWInjectionsGWFAST(injections_load,  N_gen = N_gen_tot, snr_th=snr_th)
 
         self.allPopsMD  = AllPopulations(Cosmo(dist_unit))
 
@@ -55,7 +116,7 @@ class SelectionBias():
         self.allPopsMD.set_values( jointpopMGCP )
 
 
-    def update_bias_fcn(self, param_todo, zmax):
+    def update_bias_fcn(self, param_todo, zmax, normalized):
         """Compute the bias function for a given parameter/s.
 
         Args:
@@ -73,7 +134,7 @@ class SelectionBias():
                                                   [self.injMock, ],
                                                   [self.key2MGCP[param_todo], ], 
                                                   zmax = zmax,
-                                                  normalized=True)
+                                                  normalized=normalized)
 
     def get_bias_fcn(self, x):
         """Return the bias function for a given parameter/s.
@@ -88,11 +149,11 @@ class SelectionBias():
             function: bias function
         """
 
-        return self.fcn1D.Ndet(x)[0][0]
+        return self.fcn1D.Ndet(x)
 
 
 
-    def update_bias_fcn_ND(self, param_todo, zmax):
+    def update_bias_fcn_ND(self, param_todo, zmax, normalized):
         """Compute the bias function for a given parameter/s.
 
         Args:
@@ -110,7 +171,7 @@ class SelectionBias():
                                                   [self.injMock, ],
                                                   param_todo, 
                                                   zmax = zmax,
-                                                  normalized=True)
+                                                  normalized=normalized)
 
     def get_bias_fcn_ND(self, x):
         """Return the bias function for a given parameter/s.
@@ -125,4 +186,4 @@ class SelectionBias():
             function: bias function
         """
 
-        return self.fcnND.Ndet(x)[0][0]
+        return self.fcnND.Ndet(x)
