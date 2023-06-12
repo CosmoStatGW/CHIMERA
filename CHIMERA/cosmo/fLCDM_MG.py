8f64e311bfd717c231381c0b9ba7ecc2642e68bc
Nicola Borghi <nicoborghi@outlook.it>
2023-06-12 15:33:51 +0200

    cleaned cosmology functions

--- CHIMERA/cosmo/fLCDM_MG.py
+++ CHIMERA/cosmo/fLCDM_MG.py
@@ -1,7 +1,6 @@
 import numpy as np 
-# import functools
 from scipy.interpolate import interp1d
-from scipy.special import hyp2f1
+
 
 from astropy.cosmology import scalar_inv_efuncs
 E_inv = np.vectorize(scalar_inv_efuncs.flcdm_inv_efunc_norel, otypes=[np.float64])
@@ -10,36 +9,17 @@ c_light = 299792.458 # km/s
 
 
 
-def _T_hypergeometric(x):
-    r"""TAKEN FROM ASTROPY 
-    Compute value using Gauss Hypergeometric function 2F1.
-    T(x) = 2 \sqrt(x) _{2}F_{1}\left(\frac{1}{6}, \frac{1}{2}; \frac{7}{6}; -x^3 \right)
-
-    Ref. [1] Baes, Camps, & Van De Putte (2017). MNRAS, 468(1), 927-930.
-    """
-    return 2 * np.sqrt(x) * hyp2f1(1./6, 1./2, 7./6, -x**3)
-
-def _int_dC_hyperbolic(z, Om0):
-    s = ((1 - Om0) / Om0) ** (1./3)
-    prefactor = (s * Om0)**(-0.5)
-    return prefactor * (_T_hypergeometric(s) - _T_hypergeometric(s / (z + 1.0)))
+from .fLCDM import dC
 
 
 ##########################
 ####### Distances ########
 ##########################
 
-def dC(z, args):
-    """Comoving distance [Mpc] at redshift ``z``."""
-    return c_light/args["H0"] * _int_dC_hyperbolic(z, args["Om0"])
 
-def dL(z, args):
+def dLEM(z, args):
     """Luminosity distance [Mpc] at redshift ``z``."""
     return (z+1.0)*dC(z, args)
-
-def dA(z, args):
-    """Angular diameter distance [Mpc] at redshift ``z``."""
-    return dC(z, args)/(z+1.0)
 
 
 def ddL_dz(z, args, dL=None):
@@ -50,6 +30,10 @@ def ddL_dz(z, args, dL=None):
         return dL/(1+z) + c_light/args["H0"] * (1+z) * E_inv(z, args["Om0"], 1.-args["Om0"])
 
     return dC(z, args) + c_light/args["H0"] * (1+z) * E_inv(z, args["Om0"], 1.-args["Om0"])
+
+def log_ddL_dz(z, args, dL=None):
+    """log of the differential luminosity distance [Mpc] at redshift ``z``."""
+    return np.log(ddL_dz(z, args, dL=dL))
 
 
 ##########################
@@ -78,10 +62,11 @@ def Xi(z, args):
 def sPrime(z, args):
     return Xi(z, args)-args["n"]*(1-args["Xi0"])/(1+z)**args["n"]
 
-def dL_MG(z, args):
+def dL(z, args):
     """Luminosity distance [Mpc] at redshift ``z`` in (Xi, n) modified gravity.
     """
     return dL(z, args) if ((args["Xi0"]==1) | (args["n"]==0)) else dL(z, args)*Xi(z, args)
+
 
 # def ddL_dz_MG(z):
 #     """Differential luminosity distance [Mpc] at redshift ``z`` in (Xi, n) modified gravity."""
@@ -98,7 +83,7 @@ def dL_MG(z, args):
 ### Solver for z(dL)  ###
 #########################
 
-def z_from_dL(dL_vec, args):
+def z_from_dLEM(dL_vec, args):
     z_grid  = np.concatenate([np.logspace(-15, np.log10(9.99e-09), base=10, num=10), 
                               np.logspace(-8,  np.log10(7.99), base=10, num=1000),
                               np.logspace(np.log10(8), 5, base=10, num=100)])
@@ -107,13 +92,13 @@ def z_from_dL(dL_vec, args):
                        fill_value=(0,np.NaN), assume_sorted=True)
     return f_intp(dL_vec) 
 
-# def z_from_dLGW(self, dL):
-#     z_grid  = np.concatenate([np.logspace(start=-15, stop=np.log10(9.99e-09), base=10, num=10), 
-#                                 np.logspace(start=-8, stop=np.log10(7.99), base=10, num=1000),
-#                                 np.logspace(start=np.log10(8), stop=5, base=10, num=100)])
-#     f_intp  = interp1d(self.dL(z_grid)*self.Xi(z_grid), z_grid, 
-#                         kind='cubic', bounds_error=False, fill_value=(0,np.NaN), assume_sorted=True)
-#     return f_intp(dL) 
+def z_from_dLGW(self, dL):
+    z_grid  = np.concatenate([np.logspace(start=-15, stop=np.log10(9.99e-09), base=10, num=10), 
+                                np.logspace(start=-8, stop=np.log10(7.99), base=10, num=1000),
+                                np.logspace(start=np.log10(8), stop=5, base=10, num=100)])
+    f_intp  = interp1d(self.dL(z_grid)*self.Xi(z_grid), z_grid, 
+                        kind='cubic', bounds_error=False, fill_value=(0,np.NaN), assume_sorted=True)
+    return f_intp(dL) 
 
 
 
